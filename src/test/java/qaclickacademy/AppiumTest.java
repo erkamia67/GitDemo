8385c83619e9298c7c0dca4f8de1e39a2e7cf7ea
erkam <erkamguler1903@gmail.com>
2019-12-17 18:09:02 -0500

    last try

--- src/test/java/qaclickacademy/AppiumTest.java
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -6,20 +6,8 @@ public class AppiumTest {
 
 	@Test
 	public void dsd() {
-		System.out.println("sdfgsdfgsdfgsdfgsdfgsdfgerstgsfghsdfgh");
-		System.out.println("lmao");
-		System.out.println("xdxdxdxd");
-		System.out.println("asdajskhdkasd");
-		System.out.println("fdsgjkhdfgklhdfljghdfgh");
-		System.out.println("dfkgljhdfklgjhdfkghdfgh");
-		System.out.println("dfsgsdfgsdfgsdfgsdfgdsfgsdfgsdf");
-		System.out.println("dskfgsdkfjgsdfg");
-		System.out.println("lmao xd hahaa");
-		System.out.println("sdkjflhgskdljfghskdjlfhgsdkjflghsdfkgsdfkljnhbvskdfjbnsd");
-		System.out.println("sdkjflhgskdljfghskdjlfhgsdkjflghsdfkgsdfkljnhbvskdfjbnsd");
-		System.out.println("sdkjflhgskdljfghskdjlfhgsdkjflghsdfkgsdfkljnhbvskdfjbnsd");
-		System.out.println("sdkjflhgskdljfghskdjlfhgsdkjflghsdfkgsdfkljnhbvskdfjbnsd");
-		System.out.println("sdkjflhgskdljfghskdjlfhgsdkjflghsdfkgsdfkljnhbvskdfjbnsd");
+		System.out.println("nothing is here");
+		
 		
 	}
 
